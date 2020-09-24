459808d3cbf7afa00351ac04b0b6063b315c0f98
Yang Song <ysong.sc@gmail.com>
2020-09-24 12:56:05 +0200

    70% time and 87% mem beat

---
+++ 0703_KthLargestElementinaStream.py
@@ -0,0 +1,21 @@
+import heapq 
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = []
+        self.k = k
+        for i in nums:
+            if len(self.heap) < self.k:
+                heapq.heappush(self.heap, i)
+            else:
+                if i > self.heap[0]: # smallest
+                    heapq.heappushpop(self.heap, i)
+        
+
+    def add(self, val: int) -> int:
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+        else:
+            if val > self.heap[0]:
+                heapq.heappushpop(self.heap, val)
+        return self.heap[0]
