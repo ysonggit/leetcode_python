c4de03c5d4db075d2a641e36729581bb22d93c58
Yang Song <ysong.sc@gmail.com>
2020-10-11 08:09:21 +0200

    55% time 6% mem

---
+++ 0428_SerializeandDeserializeN-aryTree.py
@@ -0,0 +1,27 @@
+class Codec:
+    def serialize(self, root: 'Node') -> str:
+        """Encodes a tree to a single string.
+        
+        :type root: Node
+        :rtype: str
+        """
+        def tuplify(root):
+            return root and (root.val, [tuplify(c) for c in root.children])
+        return json.dumps(tuplify(root))
+        
+	
+    def deserialize(self, data: str) -> 'Node':
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: Node
+        """
+        def detuplify(t):
+            if t:
+                root = Node(t[0], [])
+                for c in t[1]:
+                    root.children.append(detuplify(c))
+                return root
+            return None
+        t = json.loads(data)
+        return detuplify(t)
