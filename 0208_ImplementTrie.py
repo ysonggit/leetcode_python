69b45dd2ed8f254cc44773605a331b607ebb23e9
Yang Song <ysonggit@users.noreply.github.com>
2020-11-12 21:05:40 +0100

    75% time 5% mem

---
+++ 0208_ImplementTrie.py
@@ -0,0 +1,46 @@
+class TrieNode:
+    def __init__(self):
+        self.children= {}
+        self.word = False 
+        
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+            
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.word or len(cur.children) > 0
