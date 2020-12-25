ba5d7ed9d8020a725b632b1743cdf81be61f898a
Yang Song <ysonggit@users.noreply.github.com>
2020-12-26 00:15:25 +0100

    68% time 12% mem

--- 0498_DiagonalTraverse.py
+++ 0498_DiagonalTraverse.py
@@ -3,31 +3,19 @@ class Solution:
         if not matrix:
             return []
         m, n = len(matrix), len(matrix[0])
-        res =[None] * (m * n)
+        res =[]
+        levels = defaultdict(list)
         '''
-        If out of bottom border (row >= m) then row = m - 1; col += 2; change walk direction.
-        if out of right border (col >= n) then col = n - 1; row += 2; change walk direction.
-        if out of top border (row < 0) then row = 0; change walk direction.
-        if out of left border (col < 0) then col = 0; change walk direction.
+        level 0: [0,0]
+        level 1: [0,1],[1,0]
+        level 2: [2,0],[1,1],[0,2]
         '''
-        r, c = 0, 0
-        d = 1
-        for i in range(m * n):
-            res[i] = matrix[r][c]
-            r -= d
-            c += d
-            if r == m:
-                r = m - 1
-                c += 2
-                d = -d
-            elif c == n:
-                c = n - 1
-                r += 2
-                d = -d
-            elif r < 0:
-                r = 0
-                d = -d
-            elif c < 0: 
-                c = 0
-                d = -d
+        for i in range(m):
+            for j in range(n):
+                levels[i+j].append(matrix[i][j])
+        for l in range(len(levels)):
+            if l % 2 == 0:
+                res += levels[l][::-1]
+            else:
+                res += levels[l]
         return res
