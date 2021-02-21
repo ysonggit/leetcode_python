9cbe615b0dd67a9b76b668226077f7e9b071bceb
Yang Song <ysonggit@users.noreply.github.com>
2021-02-21 20:34:57 +0100

    57% time 19% mem

---
+++ 0991_BrokenCalculator.py
@@ -0,0 +1,10 @@
+class Solution:
+    def brokenCalc(self, X: int, Y: int) -> int: 
+        res = 0
+        while Y > X:
+            res += 1
+            if Y % 2 == 0:
+                Y //= 2
+            else:
+                Y += 1
+        return res + X - Y
